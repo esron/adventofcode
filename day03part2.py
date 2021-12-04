c24d2dab996f5b0246790a4b89b089d79f770738
Esron <esron.dtamar@gmail.com>
2021-12-04 12:49:37 -0300

    add day 03 part 2 solution

--- day03part2.py
+++ day03part2.py
@@ -5,16 +5,37 @@ input = []
 for line in stdin:
     input.append(line.rstrip())
 
-lineSize = len(input[0])
+def filter_ones(values, index):
+    return list(filter(lambda e: e[index] == '1', values))
 
-ones_zeros_count = []
+def filter_zeros(values, index):
+    return list(filter(lambda e: e[index] == '0', values))
 
-for _ in range(0, lineSize):
-    ones_zeros_count.append([0,0])
+def oxygen_generator_rating(values, index):
+    if len(values) == 1:
+        return values[0]
 
-for value in input:
-    for i in range(0, len(value.rstrip())):
-        if line[i] == '1':
-            ones_zeros_count[i][1] += 1
-        else:
-            ones_zeros_count[i][0] += 1
+    ones = filter_ones(values, index)
+    zeros = filter_zeros(values, index)
+
+    if len(ones) >= len(zeros):
+        return oxygen_generator_rating(ones, index+1)
+    else:
+        return oxygen_generator_rating(zeros, index+1)
+
+def CO2_scrubber_rating(values, index):
+    if len(values) == 1:
+        return values[0]
+
+    ones = filter_ones(values, index)
+    zeros = filter_zeros(values, index)
+
+    if len(zeros) > len(ones):
+        return CO2_scrubber_rating(ones, index+1)
+    else:
+        return CO2_scrubber_rating(zeros, index+1)
+
+ogr = int(oxygen_generator_rating(input, 0), 2)
+csr = int(CO2_scrubber_rating(input, 0), 2)
+
+print(ogr * csr)
