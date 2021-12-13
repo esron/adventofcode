602e146125e407d1bbaf3b33bd794deb15dd282e
Esron Silva <essilva@redhat.com>
2021-12-13 10:46:19 -0300

    Add day 07 part 1

---
+++ day07part1.py
@@ -0,0 +1,17 @@
+def calculate_fuel(crabs, position):
+    fuel = 0
+    for c in crabs:
+        fuel += abs(c - position)
+    return fuel
+
+crabs = list(map(int, input().split(',')))
+
+max_position = max(crabs)
+
+min_fuel = calculate_fuel(crabs, 0)
+
+for p in range(1, max_position):
+    f= calculate_fuel(crabs, p)
+    min_fuel = f if f < min_fuel else min_fuel
+
+print(min_fuel)
