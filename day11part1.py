04ef4303d07816b5ef6edff73d9937f52202e8bd
Esron Silva <essilva@redhat.com>
2021-12-20 16:32:58 -0300

    Add day 11 part 1

---
+++ day11part1.py
@@ -0,0 +1,45 @@
+from sys import stdin
+
+board = []
+steps = 100
+counter = 0
+
+def step():
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            board[i][j] += 1
+
+def in_range(x, y):
+    return (0 <= x < len(board)) and (0 <= y < len(board[x]))
+
+def increases_surroundings(i, j):
+    for ii in (-1, 0, 1):
+        for jj in (-1, 0, 1):
+            x = ii + i
+            y = jj + j
+
+            if in_range(x, y) and board[x][y] != 0:
+                board[x][y] += 1
+
+def flash():
+    global counter
+    increments = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] > 9:
+                board[i][j] = 0
+                counter += 1
+                increments.append((i, j))
+    if len(increments) > 0:
+        for i, j in increments:
+            increases_surroundings(i, j)
+        flash()
+
+for line in stdin:
+    board.append(list(map(int, line.rstrip())))
+
+for s in range(1, steps + 1):
+    step()
+    flash()
+
+print(counter)
