deb02743a836c0bd738a9c44829f637ca0815829
Esron Silva <essilva@redhat.com>
2021-12-21 14:47:57 -0300

    Add day 13 part 1

---
+++ day13part1.py
@@ -0,0 +1,56 @@
+from sys import stdin
+from typing import Iterable
+
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def distance_x(self, x: int) -> int:
+        return abs(x - self.x)
+
+    def distance_y(self, y: int) -> int:
+        return abs(y - self.y)
+
+    def move_x(self, x: int) -> None:
+        self.x = x
+
+    def move_y(self, y: int) -> None:
+        self.y = y
+
+    def __eq__(self, __o: object) -> bool:
+        return self.x == __o.x and self.y == __o.y
+
+
+def execute(instruction: str, points: Iterable[Point]) -> None:
+    axis, value = instruction.split('=')
+    value = int(value)
+
+    for p in points:
+        if axis == 'y':
+            if p.y > value:
+                p.move_y(value - p.distance_y(value))
+        if axis == 'x':
+            if p.x > value:
+                p.move_x(value - p.distance_x(value))
+
+def remove_equals(points: Iterable[Point]) -> Iterable[Point]:
+    filtered = []
+    while(len(points) > 0):
+        filtered.append(points[0])
+        points = list(filter(lambda x: x != points[0], points))
+    return filtered
+
+points = []
+instructions = []
+
+for line in stdin:
+    if len(line.split(',')) == 2:
+        points.append(Point(*tuple(map(int, line.rstrip().split(',')))))
+    elif line != '\n':
+        instructions.append(line.rstrip().split(' ')[2])
+
+execute(instructions[0], points)
+points = remove_equals(points)
+
+print(len(points))
