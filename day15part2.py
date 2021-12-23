e5489b899ee7669fddfe35a4136a16ba6ddf85c8
Esron Silva <essilva@redhat.com>
2021-12-23 17:52:16 -0300

    Add day 15 part 1 draft

---
+++ day15part2.py
@@ -0,0 +1,127 @@
+from decimal import Decimal
+from sys import path, stdin
+from typing import Iterable
+from queue import PriorityQueue
+
+class Point:
+    def __init__(self, coord: 'tuple[int, int]', value: int) -> None:
+        self.x = coord[0]
+        self.y = coord[1]
+        self.value = value
+        self.f = Decimal('Infinity')
+        self.g = Decimal('Infinity')
+        self.father: Point = None
+
+    def distance(self, point: 'Point') -> int:
+        """Manhattan distance to point"""
+        return (point.x - self.x) + (point.y - self.y)
+
+    def set_father(self, point: 'Point') -> None:
+        self.father = point
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Point):
+            return self.x == __o.x and self.y == __o.y
+        else:
+            return False
+
+    def __gt__(self, __o: object) -> bool:
+        return self.f > __o.f
+
+    def __lt__(self, __o: object) -> bool:
+        return self.f < __o.f
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def __str__(self) -> str:
+        return f'{{ x: {str(self.x)}, y: {str(self.y)}, f: {self.f}, \
+g: {self.g} value: {self.value} }}'
+
+def is_in_range(x: int, y: int, grid: Iterable[Iterable[int]]) -> bool:
+    return (0 <= x < len(grid)) and (0 <= y < len(grid[x]))
+
+def append_if_in_range(grid, n, direction):
+    if is_in_range(*direction, grid):
+        n.append(grid[direction[0]][direction[1]])
+
+def get_neighbors(point: Point, grid: Iterable[Iterable[int]]) -> Iterable[Point]:
+    n = []
+
+    append_if_in_range(grid, n, (point.x - 1, point.y))
+    append_if_in_range(grid, n, (point.x, point.y - 1))
+    append_if_in_range(grid, n, (point.x + 1, point.y))
+    append_if_in_range(grid, n, (point.x, point.y + 1))
+
+    return n
+
+def reconstruct_path(p: Point) -> Iterable[Point]:
+    path = []
+    while p != None:
+        path.append(p)
+        p = p.father
+    return path
+
+def pathfinder(A: Point, goal: Point,
+               grid: Iterable[Iterable[int]]) -> Iterable[Point]:
+    open_list = PriorityQueue()
+    open_list.put(A)
+    come_from = {A: None}
+    i = 1
+    while not open_list.empty():
+        if i % 500 == 0:
+            print('Iteration: ', i)
+        i += 1
+        current = open_list.get()
+
+        if current == goal:
+            return reconstruct_path(current)
+
+        neighbors = get_neighbors(current, grid)
+
+        for n in neighbors:
+            if n in come_from:
+                continue
+
+            new_g = current.g + n.value
+
+            if new_g < n.g:
+                n.father = current
+                come_from[n] = current
+                n.g = new_g
+                n.f = new_g + n.distance(goal)
+                open_list.put(n)
+
+def increment(x: int) -> int:
+    return x % 9 + 1
+
+grid = []
+f = open('day15.txt')
+for line in stdin:
+    l = list(map(int, line.rstrip()))
+    ll = l.copy()
+    for _ in range(4):
+        l = list(map(increment, l))
+        ll += l
+
+    grid.append(ll)
+
+g = grid.copy()
+for _ in range(4):
+    for i in range(len(g)):
+        g[i] = list(map(increment, g[i]))
+
+    grid += g
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        grid[i][j] = Point((i, j), grid[i][j])
+
+start = grid[0][0]
+start.g = 0
+start.f = 0
+goal  = grid[len(grid) - 1][len(grid[0]) - 1]
+
+path = pathfinder(start, goal, grid)
+
+print(path[0].f)
