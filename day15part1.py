fb96858bf01d956ea013c4b63e9d19c7a6cc680a
Esron <esron.dtamar@gmail.com>
2021-12-22 21:11:40 -0300

    Day 15 part 1 draft 2

--- day15part1.py
+++ day15part1.py
@@ -1,14 +1,14 @@
-from _typeshed import Self
+from decimal import Decimal
 from sys import stdin
 from typing import Iterable
 
 class Point:
-    def __init__(self, coord: tuple[int, int], value: int, cost_to_move: int, weight: int) -> None:
+    def __init__(self, coord: 'tuple[int, int]', value: int) -> None:
         self.x = coord[0]
         self.y = coord[1]
         self.value = value
-        self.cost_to_move = cost_to_move
-        self.weight = weight
+        self.f = Decimal('Infinity')
+        self.g = Decimal('Infinity')
         self.father: Point = None
 
     def distance(self, point: 'Point') -> int:
@@ -22,12 +22,14 @@ class Point:
         return self.x == __o.x and self.y == __o.y
 
     def __gt__(self, __o: object) -> bool:
-        return self.weight > __o.weight
+        return self.f > __o.f
 
     def __lt__(self, __o: object) -> bool:
-        return self.weight < __o.weight
+        return self.f < __o.f
 
-
+    def __str__(self) -> str:
+        return f'{{ x: {str(self.x)}, y: {str(self.y)}, weight: {self.weight} \
+value: {self.value} cost_to_move: {self.cost_to_move} }}'
 
 grid = []
 f = open('testinput.txt')
@@ -38,13 +40,61 @@ for line in f:
 def is_in_range(x: int, y: int, grid: Iterable[Iterable[int]]) -> bool:
     return (0 <= x < len(grid)) and (0 <= y < len(grid[x]))
 
-start = (0, 0)
-goal = (len(grid), len(grid[0]))
+def append_if_in_range(grid, n, direction):
+    if is_in_range(*direction, grid):
+        n.append(Point(direction, grid[direction[0]][direction[1]]))
 
-def pathfinder(A: Point, grid: Iterable[Iterable[int]]) -> Iterable[Point]:
+def get_neighbors(point: Point, grid: Iterable[Iterable[int]]) -> Iterable[Point]:
+    n = []
+
+    append_if_in_range(grid, n, (point.x - 1, point.y))
+    append_if_in_range(grid, n, (point.x, point.y - 1))
+    append_if_in_range(grid, n, (point.x + 1, point.y))
+    append_if_in_range(grid, n, (point.x, point.y + 1))
+
+    return n
+
+def reconstruct_path(p: Point) -> Iterable[Point]:
+    path = []
+    while p.father != None:
+        path.append(p)
+        p = p.father
+    return path
+
+def pathfinder(A: Point, goal: Point,
+               grid: Iterable[Iterable[int]]) -> Iterable[Point]:
     open_list = [A]
     closed_list = []
 
     while(len(open_list) != 0):
         open_list = sorted(open_list, reverse=True)
         current = open_list.pop()
+
+        closed_list.append(current)
+        if current == goal:
+            return reconstruct_path(current)
+
+        neighbors = get_neighbors(current, grid)
+
+        for n in neighbors:
+            if n in closed_list:
+                continue
+
+            new_g = current.g + n.value
+
+            if new_g < n.g:
+                n.father = current
+                n.g = new_g
+                n.f = new_g + n.distance(goal)
+
+                if n not in open_list:
+                    open_list.append(n)
+
+
+start = Point((0, 0), grid[0][0])
+start.g = 0
+start.f = 0
+goal  = Point((len(grid), len(grid[0])), grid[len(grid) - 1][len(grid[0]) - 1])
+
+path = pathfinder(start, goal, grid)
+print(path)
