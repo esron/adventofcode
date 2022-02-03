a8d7f191eda615a1c638262112feb657747f67d6
Esron Silva <essilva@redhat.com>
2022-02-03 17:23:56 -0300

    Abstract hex to bin implementation

--- day16part1.py
+++ day16part1.py
@@ -1,6 +1,11 @@
 input  = 'D2FE28'
 
-bin_string = '{0:b}'.format(int(input, 16))
+def hex_to_bin(input: str) -> str:
+    """Convert from hex string to binary string"""
+    return '{0:b}'.format(int(input, 16))
+
+bin_string = hex_to_bin(input)
+
 print(bin_string)
 V = bin_string[:3]
 print(V)
