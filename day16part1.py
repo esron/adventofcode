e2aab3dd6911ef937c938e49cb679653d3070ced
Esron <esron.dtamar@gmail.com>
2022-02-07 08:46:57 -0300

    Add metods for a literal value

--- day16part1.py
+++ day16part1.py
@@ -1,22 +1,48 @@
 from collections import deque
-input  = 'D2FE28'
+input  = '38006F45291200'
 
 def hex_to_bin(input: str) -> str:
     """Convert from hex string to binary string"""
     return '{0:b}'.format(int(input, 16))
 
 def take_from_queue(bin_queue: deque, times: int) -> list:
-    version_id = []
+    response = []
     for _ in range(times):
-        version_id.append(bin_queue.popleft())
-    return version_id
+        response.append(bin_queue.popleft())
+    return response
+
+def take_package_size(bin_queue: deque, size_bit: str) -> list:
+    if size_bit == '1':
+        return take_from_queue(bin_queue, 11)
+    return take_from_queue(bin_queue, 15)
+
+def take_literal_value(bin_queue: deque) -> list:
+    response = []
+    while(True):
+        five_bits = take_from_queue(bin_queue, 5)
+
+        response += five_bits[1:]
+        if(five_bits[0] == '0'):
+            break
+    return response
 
 bin_string = hex_to_bin(input)
 
 print(bin_string)
+
 bin_queue = deque(list(bin_string))
 
-version_id = ''.join(take_from_queue(bin_queue, 3))
-print('{:<12} {:<3}'.format('version_id', int(version_id, 2)))
-type_id = ''.join(take_from_queue(bin_queue, 3))
-print('{:<12} {:<3}'.format('type_id', int(type_id, 2)))
+total_version_id = 0
+while bin_queue:
+    version_id = ''.join(take_from_queue(bin_queue, 3))
+    total_version_id += int(version_id, 2)
+    print('{:<20} {:<10}'.format('version_id', int(version_id, 2)))
+    print('{:<20} {:<10}'.format('total_version_id', total_version_id))
+    type_id = ''.join(take_from_queue(bin_queue, 3))
+    print('{:<20} {:<10}'.format('type_id', int(type_id, 2)))
+    if int(type_id, 2) == 4:
+        literal = take_literal_value(bin_queue)
+        print(literal)
+        print(int(''.join(literal), 2))
+    while bin_queue and bin_queue[-1] == '0':
+        bin_queue.popleft()
