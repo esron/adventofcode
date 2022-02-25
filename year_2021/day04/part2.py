d80a0f576e94dc7b59b6c89bd1f4beefb24c482c
Esron Silva <essilva@redhat.com>
2022-02-25 10:56:00 -0300

    Refactor to use the run method

--- year_2021/day04/part2.py
+++ year_2021/day04/part2.py
@@ -1,52 +1,62 @@
-from sys import stdin
+import os
+import click
 
-numbers = input()
-
-numbers = numbers.rstrip().split(',')
-
-boards = []
-board = []
-input()
-for line in stdin:
-  if line == '\n':
-    boards.append(board)
-    board = []
-    continue
-  board.append(list(filter(lambda e: e != '', line.strip().split(' '))))
-
-boards.append(board)
 
 def bingo(board):
-  for i in range(0, len(board)):
-    colum_count = 0
-    line_count = 0
-    for j in range(0, len(board[i])):
-      if board[i][j] == 'X':
-        line_count += 1
-      if line_count == 5:
-        return True
-      if board[j][i] == 'X':
-        colum_count += 1
-      if colum_count == 5:
-        return True
-  return False
+    for i in range(0, len(board)):
+        colum_count = 0
+        line_count = 0
+        for j in range(0, len(board[i])):
+            if board[i][j] == 'X':
+                line_count += 1
+            if line_count == 5:
+                return True
+            if board[j][i] == 'X':
+                colum_count += 1
+            if colum_count == 5:
+                return True
+    return False
+
 
 def score(board):
-  score = 0
-  for line in board:
-    score += sum(map(lambda x: int(x) if x != 'X' else 0, line))
-  return score
+    score = 0
+    for line in board:
+        score += sum(map(lambda x: int(x) if x != 'X' else 0, line))
+    return score
 
-for number in numbers:
-  for b in range(0, len(boards)):
-    for i in range(0, len(boards[b])):
-      for j in range(0, len(boards[b][i])):
-        if boards[b][i][j] == number:
-          boards[b][i][j]  = 'X'
 
-  if len(boards) != 1:
-    boards = list(filter(lambda b: not bingo(b), boards))
-  else:
-    if bingo(boards[0]):
-      print(score(boards[0]) * int(number))
-      exit(0)
+def run():
+    f = open(os.getcwd() + '/year_2021/day04/input.txt')
+    numbers = f.readline()
+
+    numbers = numbers.rstrip().split(',')
+
+    boards = []
+    board = []
+    f.readline()
+    for line in f:
+        if line == '\n':
+            boards.append(board)
+            board = []
+            continue
+        board.append(list(filter(lambda e: e != '', line.strip().split(' '))))
+
+    boards.append(board)
+
+    for number in numbers:
+        for b in range(0, len(boards)):
+            for i in range(0, len(boards[b])):
+                for j in range(0, len(boards[b][i])):
+                    if boards[b][i][j] == number:
+                        boards[b][i][j] = 'X'
+
+        if len(boards) != 1:
+            boards = list(filter(lambda b: not bingo(b), boards))
+        else:
+            if bingo(boards[0]):
+                print(score(boards[0]) * int(number))
+                return
+
+
+if __name__ == "__main__":
+    run()
