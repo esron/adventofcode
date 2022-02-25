904700d1ffc95a74f5910ad4e78252300a16ef5b
Esron Silva <essilva@redhat.com>
2022-02-25 11:52:20 -0300

    Refactor to use run method

--- year_2021/day06/part2.py
+++ year_2021/day06/part2.py
@@ -1,12 +1,7 @@
+import os
+import click
 from collections import deque
 
-input = input()
-input = list(map(int, input.rstrip().split(',')))
-
-fishes = [0] * 9
-
-for i in input:
-    fishes[int(i)] += 1
 
 def count_fish(l, days):
     q = deque(l)
@@ -17,4 +12,19 @@ def count_fish(l, days):
         q.append(spawn)
     return sum(q)
 
-print(count_fish(fishes, 256))
+
+def run():
+    f = open(os.getcwd() + '/year_2021/day06/input.txt')
+    input = f.readline()
+    input = list(map(int, input.rstrip().split(',')))
+
+    fishes = [0] * 9
+
+    for i in input:
+        fishes[int(i)] += 1
+
+    click.echo(count_fish(fishes, 256))
+
+
+if __name__ == "__main__":
+    run()
