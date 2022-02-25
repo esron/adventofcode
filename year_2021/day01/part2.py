0b7693cad5fe8db693d56c3067f0f08b7b7de9ce
Esron Silva <essilva@redhat.com>
2022-02-25 10:01:44 -0300

    Use run function

--- year_2021/day01/part2.py
+++ year_2021/day01/part2.py
@@ -1,23 +1,31 @@
-increased = 0
-values = []
+import os
+import click
 
-f = open('input.txt', 'r')
 
-for line in f:
-    values.append(int(line))
+def run():
+    f = open(os.getcwd() + '/year_2021/day01/input.txt')
+    increased = 0
+    values = []
 
-firstWindown = values[0] + values[1] + values[2]
-secondWindown = values[1] + values[2] + values[3]
-for i in range(2, len(values) - 2):
-    thirdWindow = values[i] + values[i+1] + values[i+2]
+    for line in f:
+        values.append(int(line))
+
+    firstWindown = values[0] + values[1] + values[2]
+    secondWindown = values[1] + values[2] + values[3]
+    for i in range(2, len(values) - 2):
+        thirdWindow = values[i] + values[i+1] + values[i+2]
+
+        if secondWindown > firstWindown:
+            increased += 1
+
+        firstWindown = secondWindown
+        secondWindown = thirdWindow
 
     if secondWindown > firstWindown:
         increased += 1
 
-    firstWindown = secondWindown
-    secondWindown = thirdWindow
+    click.echo(increased)
 
-if secondWindown > firstWindown:
-    increased += 1
 
-print(increased)
+if __name__ == "__main__":
+    run()
