6047c88ae9c65b5695f887d56f3aa4bc5df9b851
Esron Silva <essilva@redhat.com>
2022-02-25 11:59:40 -0300

    Refactor to use run method

--- year_2021/day07/part1.py
+++ year_2021/day07/part1.py
@@ -1,17 +1,28 @@
+import os
+import click
+
+
 def calculate_fuel(crabs, position):
     fuel = 0
     for c in crabs:
         fuel += abs(c - position)
     return fuel
 
-crabs = list(map(int, input().split(',')))
 
-max_position = max(crabs)
+def run():
+    f = open(os.getcwd() + '/year_2021/day07/input.txt')
+    crabs = list(map(int, f.readline().split(',')))
 
-min_fuel = calculate_fuel(crabs, 0)
+    max_position = max(crabs)
 
-for p in range(1, max_position):
-    f= calculate_fuel(crabs, p)
-    min_fuel = f if f < min_fuel else min_fuel
+    min_fuel = calculate_fuel(crabs, 0)
 
-print(min_fuel)
+    for p in range(1, max_position):
+        f = calculate_fuel(crabs, p)
+        min_fuel = f if f < min_fuel else min_fuel
+
+    click.echo(min_fuel)
+
+
+if __name__ == "__main__":
+    run()
