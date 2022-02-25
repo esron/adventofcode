b3fa978c482ff3f9563a726f77dd14b71823862d
Esron Silva <essilva@redhat.com>
2022-02-25 11:30:19 -0300

    Refactor to use run method

--- year_2021/day05/part1.py
+++ year_2021/day05/part1.py
@@ -1,33 +1,41 @@
-from sys import stdin
+import os
+import click
 
-diagram_size = 1000
-diagram = []
-for i in range(diagram_size):
-  diagram.append([0] * diagram_size)
 
-for line in stdin:
-  p1, _, p2 = line.rstrip().split(' ')
+def run():
+    f = open(os.getcwd() + '/year_2021/day05/input.txt')
+    diagram_size = 1000
+    diagram = []
+    for i in range(diagram_size):
+        diagram.append([0] * diagram_size)
 
-  x1, x2 = list(map(int, p1.split(',')))
-  y1, y2 = list(map(int, p2.split(',')))
+    for line in f:
+        p1, _, p2 = line.rstrip().split(' ')
 
-  if x1 == y1:
-    if x2 > y2:
-      for i in range(y2, x2 + 1):
-        diagram[i][x1] += 1
-    else:
-      for i in range(x2, y2 + 1):
-        diagram[i][x1] += 1
-  if x2 == y2:
-    if x1 > y1:
-      for i in range(y1, x1 + 1):
-        diagram[x2][i] += 1
-    else:
-      for i in range(x1, y1 + 1):
-        diagram[x2][i] += 1
+        x1, x2 = list(map(int, p1.split(',')))
+        y1, y2 = list(map(int, p2.split(',')))
 
-gt_2 = 0
-for line in diagram:
-  gt_2 += len(list(filter(lambda x: x >= 2, line)))
+        if x1 == y1:
+            if x2 > y2:
+                for i in range(y2, x2 + 1):
+                    diagram[i][x1] += 1
+            else:
+                for i in range(x2, y2 + 1):
+                    diagram[i][x1] += 1
+        if x2 == y2:
+            if x1 > y1:
+                for i in range(y1, x1 + 1):
+                    diagram[x2][i] += 1
+            else:
+                for i in range(x1, y1 + 1):
+                    diagram[x2][i] += 1
 
-print(gt_2)
+    gt_2 = 0
+    for line in diagram:
+        gt_2 += len(list(filter(lambda x: x >= 2, line)))
+
+    click.echo(gt_2)
+
+
+if __name__ == "__main__":
+    run()
