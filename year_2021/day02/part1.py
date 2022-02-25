84fd8f8425441e1b787d3f6c43e26cc427242c97
Esron Silva <essilva@redhat.com>
2022-02-25 10:08:37 -0300

    Refactor to use file instead of stdin

--- year_2021/day02/part1.py
+++ year_2021/day02/part1.py
@@ -1,15 +1,23 @@
-from sys import stdin
+import os
+import click
 
-depth = 0
-position = 0
 
-for line in stdin:
-    comand, value  = line.rstrip().split(' ')
-    if comand == 'forward':
-        position += int(value)
-    if comand == 'down':
-        depth += int(value)
-    if comand == 'up':
-        depth -= int(value)
+def run():
+    f = open(os.getcwd() + '/year_2021/day02/input.txt')
+    depth = 0
+    position = 0
 
-print(depth * position)
+    for line in f:
+        comand, value = line.rstrip().split(' ')
+        if comand == 'forward':
+            position += int(value)
+        if comand == 'down':
+            depth += int(value)
+        if comand == 'up':
+            depth -= int(value)
+
+    click.echo(depth * position)
+
+
+if __name__ == "__main__":
+    run()
