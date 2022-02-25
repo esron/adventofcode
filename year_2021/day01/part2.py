d4448e248159e900cc6ca54cb3779bb551b062d6
Esron Silva <esron.dtamar@gmail.com>
2022-02-24 23:45:26 -0300

    Format document

--- year_2021/day01/part2.py
+++ year_2021/day01/part2.py
@@ -1,23 +1,23 @@
 increased = 0
 values = []
 
-f = open('day01.txt', 'r')
+f = open('input.txt', 'r')
 
 for line in f:
-  values.append(int(line))
+    values.append(int(line))
 
 firstWindown = values[0] + values[1] + values[2]
 secondWindown = values[1] + values[2] + values[3]
 for i in range(2, len(values) - 2):
-  thirdWindow = values[i] + values[i+1] + values[i+2]
+    thirdWindow = values[i] + values[i+1] + values[i+2]
 
-  if secondWindown > firstWindown:
-    increased += 1
+    if secondWindown > firstWindown:
+        increased += 1
 
-  firstWindown = secondWindown
-  secondWindown = thirdWindow
+    firstWindown = secondWindown
+    secondWindown = thirdWindow
 
 if secondWindown > firstWindown:
-  increased += 1
+    increased += 1
 
 print(increased)
