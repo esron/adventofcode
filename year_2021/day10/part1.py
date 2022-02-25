7788bf7293462bfe4519012ce66bdf0c11a0084d
Esron Silva <essilva@redhat.com>
2022-02-25 12:41:10 -0300

    Refactor to use the run method

--- year_2021/day10/part1.py
+++ year_2021/day10/part1.py
@@ -1,26 +1,33 @@
+import os
+import click
 from collections import deque
-from sys import stdin
 
-score = 0
 
-closing = {
-  ')': ['(', 3],
-  ']': ['[', 57],
-  '}': ['{', 1197],
-  '>': ['<', 25137],
-}
+def run():
+    f = open(os.getcwd() + '/year_2021/day10/input.txt')
+    closing = {
+        ')': ['(', 3],
+        ']': ['[', 57],
+        '}': ['{', 1197],
+        '>': ['<', 25137],
+    }
 
-for line in stdin:
-  line = line.rstrip()
-  q = deque()
+    score = 0
+    for line in f:
+        line = line.rstrip()
+        q = deque()
 
-  for c in line:
-    if c in ('(', '[', '{', '<'):
-      q.append(c)
-    elif q[-1] == closing[c][0]:
-      q.pop()
-    else:
-      score += closing[c][1]
-      break
+        for c in line:
+            if c in ('(', '[', '{', '<'):
+                q.append(c)
+            elif q[-1] == closing[c][0]:
+                q.pop()
+            else:
+                score += closing[c][1]
+                break
 
-print(score)
+    click.echo(score)
+
+
+if __name__ == "__main__":
+    run()
