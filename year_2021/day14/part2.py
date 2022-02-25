a3e08e957e8c8af6ae150eea7795b9858e52e369
Esron Silva <essilva@redhat.com>
2022-02-25 16:52:19 -0300

    Refactor to use the run method

--- year_2021/day14/part2.py
+++ year_2021/day14/part2.py
@@ -1,39 +1,50 @@
-from sys import stdin
+import os
+import click
 from typing import Iterable, Mapping, Union
 
-polymer_template = input().rstrip()
 
-# Skip empty line
-input()
-pairs: Mapping[str, Iterable[Union[str, int]]] = {}
-element_counts: Mapping[str, int] = {}
-for line in stdin:
-    pair, element = line.rstrip().split(' -> ')
+def run():
+    f = open(os.getcwd() + '/year_2021/day14/input.txt')
 
-    # i.e. NN -> C: {'NN': ['NC', 'CN', 0]}
-    pairs[pair] = [pair[0] + element, element + pair[1], 0]
-    if not element in element_counts.keys():
-        element_counts[element] = 0
+    polymer_template = f.readline().rstrip()
 
-for i in range(len(polymer_template) - 1):
-    pairs[polymer_template[i] + polymer_template[i + 1]][2] += 1
+    # Skip empty line
+    f.readline()
+    pairs: Mapping[str, Iterable[Union[str, int]]] = {}
+    element_counts: Mapping[str, int] = {}
+    for line in f:
+        pair, element = line.rstrip().split(' -> ')
 
-for _ in range(1, 40):
-    to_increment = []
-    for key in pairs:
-        to_increment.append((pairs[key][0], pairs[key][2]))
-        to_increment.append((pairs[key][1], pairs[key][2]))
-        pairs[key][2] -= pairs[key][2]
-    for key, value in to_increment:
-        pairs[key][2] += value
+        # i.e. NN -> C: {'NN': ['NC', 'CN', 0]}
+        pairs[pair] = [pair[0] + element, element + pair[1], 0]
+        if element not in element_counts.keys():
+            element_counts[element] = 0
 
-for p in pairs.values():
-    element_counts[p[0][0]] += p[2]
-    element_counts[p[0][1]] += p[2]
+    for i in range(len(polymer_template) - 1):
+        pairs[polymer_template[i] + polymer_template[i + 1]][2] += 1
 
-# Hack :)
-element_counts[polymer_template[-1]] += 1
+    for _ in range(1, 40):
+        to_increment = []
+        for key in pairs:
+            to_increment.append((pairs[key][0], pairs[key][2]))
+            to_increment.append((pairs[key][1], pairs[key][2]))
+            pairs[key][2] -= pairs[key][2]
+        for key, value in to_increment:
+            pairs[key][2] += value
 
-element_counts = {k: v for k, v in sorted(element_counts.items(), key=lambda item: item[1])}
+    for p in pairs.values():
+        element_counts[p[0][0]] += p[2]
+        element_counts[p[0][1]] += p[2]
 
-print(element_counts[list(element_counts.keys())[-1]] - element_counts[list(element_counts.keys())[0]])
+    # Hack :)
+    element_counts[polymer_template[-1]] += 1
+
+    element_counts = {k: v for k, v in sorted(
+        element_counts.items(), key=lambda item: item[1])}
+
+    click.echo(element_counts[list(element_counts.keys())[-1]] -
+               element_counts[list(element_counts.keys())[0]])
+
+
+if __name__ == "__main__":
+    run()
