b796f6b8cf21438128b88239d63d3dc49a72b4e6
Esron Silva <essilva@redhat.com>
2022-02-25 11:32:15 -0300

    Refactor to use run method

--- year_2021/day05/part2.py
+++ year_2021/day05/part2.py
@@ -1,62 +1,63 @@
-from sys import stdin
+import os
+import click
 
-diagram_size = 1000
-diagram = []
-for i in range(diagram_size):
-  diagram.append([0] * diagram_size)
 
-for line in stdin:
-  p1, _, p2 = line.rstrip().split(' ')
+def run():
+    f = open(os.getcwd() + '/year_2021/day05/input.txt')
+    diagram_size = 1000
+    diagram = []
+    for i in range(diagram_size):
+        diagram.append([0] * diagram_size)
 
-  x1, y1 = list(map(int, p1.split(',')))
-  x2, y2 = list(map(int, p2.split(',')))
+    for line in f:
+        p1, _, p2 = line.rstrip().split(' ')
 
-  if x1 == x2:
-    if y1 > y2:
-      for i in range(y2, y1 + 1):
-        diagram[i][x1] += 1
-    else:
-      for i in range(y1, y2 + 1):
-        diagram[i][x1] += 1
-    continue
-  if y1 == y2:
-    if x1 > x2:
-      for i in range(x2, x1 + 1):
-        diagram[y2][i] += 1
-    else:
-      for i in range(x1, x2 + 1):
-        diagram[y2][i] += 1
-    continue
-  i = x1
-  j = y1
-  if x2 > x1:
-    if y2 > y1:
-      while i <= x2:
-        diagram[j][i] += 1
-        i += 1
-        j += 1
-    else:
-      while i <= x2:
-        diagram[j][i] += 1
-        i += 1
-        j -= 1
-  else:
-    if y2 > y1:
-      while i >= x2:
-        diagram[j][i] += 1
-        i -= 1
-        j += 1
-    else:
-      while i >= x2:
-        diagram[j][i] += 1
-        i -= 1
-        j -= 1
+        x1, y1 = list(map(int, p1.split(',')))
+        x2, y2 = list(map(int, p2.split(',')))
 
-# for line in diagram:
-#   print(''.join(list(map(str, line))))
+        if x1 == x2:
+            if y1 > y2:
+                for i in range(y2, y1 + 1):
+                    diagram[i][x1] += 1
+            else:
+                for i in range(y1, y2 + 1):
+                    diagram[i][x1] += 1
+            continue
+        if y1 == y2:
+            if x1 > x2:
+                for i in range(x2, x1 + 1):
+                    diagram[y2][i] += 1
+            else:
+                for i in range(x1, x2 + 1):
+                    diagram[y2][i] += 1
+            continue
+        i = x1
+        j = y1
+        if x2 > x1:
+            if y2 > y1:
+                while i <= x2:
+                    diagram[j][i] += 1
+                    i += 1
+                    j += 1
+            else:
+                while i <= x2:
+                    diagram[j][i] += 1
+                    i += 1
+                    j -= 1
+        else:
+            if y2 > y1:
+                while i >= x2:
+                    diagram[j][i] += 1
+                    i -= 1
+                    j += 1
+            else:
+                while i >= x2:
+                    diagram[j][i] += 1
+                    i -= 1
+                    j -= 1
 
-gt_2 = 0
-for line in diagram:
-  gt_2 += len(list(filter(lambda x: x >= 2, line)))
+    gt_2 = 0
+    for line in diagram:
+        gt_2 += len(list(filter(lambda x: x >= 2, line)))
 
-print(gt_2)
+    click.echo(gt_2)
