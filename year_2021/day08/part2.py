a731bf0dd071de4086978e896105e7da37546a97
Esron Silva <essilva@redhat.com>
2022-02-25 12:22:25 -0300

    Refactor to use run method

--- year_2021/day08/part2.py
+++ year_2021/day08/part2.py
@@ -1,9 +1,10 @@
-from sys import stdin
+import os
+import click
 
-total = 0
 
 def sort_and_split(input):
     return [''.join(sorted(s)) for s in input.strip().split(' ')]
+
 
 def easy_numbers(numbers, input):
     for s in input:
@@ -18,45 +19,57 @@ def easy_numbers(numbers, input):
 
     return numbers
 
+
 def calculate_line(numbers, output):
     total = ''
     for s in output:
         total += str(numbers.index(s))
     return int(total)
 
+
 def doesnt_have_one(one, input):
     return not one[0] in input or not one[1] in input
 
+
 def has_number(number, input):
     for c in number:
-        if not c in input:
+        if c not in input:
             return False
     return True
 
-for line in stdin:
-    numbers = [''] * 10
-    input, output = line.rstrip().split('|')
-    input = sort_and_split(input)
-    output = sort_and_split(output)
 
-    numbers = easy_numbers(numbers, input)
+def run():
+    f = open(os.getcwd() + '/year_2021/day08/input.txt')
 
-    for i in input:
-        if len(i) == 6:
-            if doesnt_have_one(numbers[1], i):
-                numbers[6] = i
-            elif has_number(numbers[4], i):
-                numbers[9] = i
-            else:
-                numbers[0] = i
-    for i in input:
-        if len(i) == 5:
-            if has_number(numbers[1], i):
-                numbers[3] = i
-            elif has_number(i, numbers[6]):
-                numbers[5] = i
-            else:
-                numbers[2] = i
-    total += calculate_line(numbers, output)
+    total = 0
+    for line in f:
+        numbers = [''] * 10
+        input, output = line.rstrip().split('|')
+        input = sort_and_split(input)
+        output = sort_and_split(output)
 
-print(total)
+        numbers = easy_numbers(numbers, input)
+
+        for i in input:
+            if len(i) == 6:
+                if doesnt_have_one(numbers[1], i):
+                    numbers[6] = i
+                elif has_number(numbers[4], i):
+                    numbers[9] = i
+                else:
+                    numbers[0] = i
+        for i in input:
+            if len(i) == 5:
+                if has_number(numbers[1], i):
+                    numbers[3] = i
+                elif has_number(i, numbers[6]):
+                    numbers[5] = i
+                else:
+                    numbers[2] = i
+        total += calculate_line(numbers, output)
+
+    click.echo(total)
+
+
+if __name__ == "__main__":
+    run()
