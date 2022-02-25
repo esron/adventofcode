cd3697da9166dc4fab771ee1252298d80e486c70
Esron Silva <essilva@redhat.com>
2022-02-25 11:50:15 -0300

    Refactor to use run method

--- year_2021/day06/part1.py
+++ year_2021/day06/part1.py
@@ -1,18 +1,26 @@
-from sys import stdin
+import os
+import click
 
-fishes = input()
 
-fishes = list(map(int, fishes.rstrip().split(',')))
+def run():
+    f = open(os.getcwd() + '/year_2021/day06/input.txt')
+    fishes = f.readline()
 
-new_fishes = []
-for _ in range(80):
-    for i in range(len(fishes)):
-        if fishes[i] == 0:
-            new_fishes.append(8)
-            fishes[i] = 6
-            continue
-        fishes[i] -= 1
-    fishes.extend(new_fishes)
+    fishes = list(map(int, fishes.rstrip().split(',')))
+
     new_fishes = []
+    for _ in range(80):
+        for i in range(len(fishes)):
+            if fishes[i] == 0:
+                new_fishes.append(8)
+                fishes[i] = 6
+                continue
+            fishes[i] -= 1
+        fishes.extend(new_fishes)
+        new_fishes = []
 
-print(len(fishes))
+    click.echo(len(fishes))
+
+
+if __name__ == "__main__":
+    run()
