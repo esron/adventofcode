f8b94017d49d49ad1380b6732a776ca71314d05a
Esron Silva <essilva@redhat.com>
2022-12-01 19:18:26 -0300

    Add 2022 day 1 part1

---
+++ year_2022/day01/part1.py
@@ -0,0 +1,24 @@
+import os
+import click
+
+
+def run():
+    file = open(os.getcwd() + '/year_2022/day01/input.txt')
+    lines = []
+    for line in file:
+        lines.append(line.rstrip())
+
+    max = 0
+    sum = 0
+    for line in lines:
+        if (line == ''):
+            if (sum > max):
+                max = sum
+            sum = 0
+            continue
+        sum += int(line)
+    click.echo(max)
+
+
+if __name__ == "__main__":
+    run()
