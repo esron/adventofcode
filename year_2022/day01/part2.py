1be491151c3d5b281b9443a17008507f989e5811
Esron Silva <essilva@redhat.com>
2022-12-01 19:18:39 -0300

    Add 2022 day 1 part 2

---
+++ year_2022/day01/part2.py
@@ -0,0 +1,24 @@
+import os
+import click
+
+
+def run():
+    file = open(os.getcwd() + '/year_2022/day01/input.txt')
+    lines = []
+    for line in file:
+        lines.append(line.rstrip())
+
+    sums = []
+    s = 0
+    for line in lines:
+        if (line == ''):
+            sums.append(s)
+            s = 0
+            continue
+        s += int(line)
+    sums.sort(reverse=True)
+    click.echo(sum(sums[:3]))
+
+
+if __name__ == "__main__":
+    run()
