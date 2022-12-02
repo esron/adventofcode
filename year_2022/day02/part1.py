41683ee69a46d792063aea353deeedd5257d4656
Esron Silva <esron.dtamar@gmail.com>
2022-12-02 19:49:50 -0300

    Add 2022 day 2 part 1 solution

---
+++ year_2022/day02/part1.py
@@ -0,0 +1,54 @@
+import os
+
+import click
+
+OP_ROCK = 'A'
+OP_PAPPER = 'B'
+OP_SCISSORS = 'C'
+MY_ROCK = 'X'
+MY_PAPPER = 'Y'
+MY_SCISSORS = 'Z'
+
+
+prs_map = {
+    OP_ROCK: {
+        MY_ROCK: 'draw',
+        MY_PAPPER: 'win',
+        MY_SCISSORS: 'loose',
+    },
+    OP_PAPPER: {
+        MY_ROCK: 'loose',
+        MY_PAPPER: 'draw',
+        MY_SCISSORS: 'win',
+    },
+    OP_SCISSORS: {
+        MY_ROCK: 'win',
+        MY_PAPPER: 'loose',
+        MY_SCISSORS: 'draw',
+    },
+}
+
+points_map = {
+    MY_ROCK: 1,
+    MY_PAPPER: 2,
+    MY_SCISSORS: 3,
+}
+
+outcome_map = {
+    'loose': 0,
+    'draw': 3,
+    'win': 6,
+}
+
+def run():
+    points = 0
+    f = open(os.getcwd() + '/year_2022/day02/input.txt')
+    for line in f:
+        op_play, my_play = line.rstrip().split(' ')
+        outcome = prs_map[op_play][my_play]
+        points += (outcome_map[outcome] + points_map[my_play])
+    click.echo(points)
+
+
+if __name__ == "__main__":
+    run()
