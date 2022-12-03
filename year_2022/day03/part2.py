1753d81d167192cf78ac63230e476bcca49b6e3e
Esron Silva <essilva@redhat.com>
2022-12-03 18:34:04 -0300

    Add 2022 day 3 part 2 solution

---
+++ year_2022/day03/part2.py
@@ -0,0 +1,24 @@
+import os
+import click
+
+
+def run():
+    f = open(os.getcwd() + '/year_2022/day03/input.txt')
+    priority_sum = 0
+    elf_a = set(f.readline().rstrip())
+    elf_b = set(f.readline().rstrip())
+    elf_c = set(f.readline().rstrip())
+
+    while(elf_a != set()):
+        intersect = elf_a.intersection(elf_b)
+        intersect = intersect.intersection(elf_c)
+        item = ''.join(intersect)
+        priority_sum += ord(item) - 38 if item.isupper() else ord(item) - 96
+        elf_a = set(f.readline().rstrip())
+        elf_b = set(f.readline().rstrip())
+        elf_c = set(f.readline().rstrip())
+    click.echo(priority_sum)
+
+
+if __name__ == "__main__":
+    run()
