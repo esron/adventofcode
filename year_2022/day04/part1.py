33b897ed113a04f301aa7f460ccf279812edde31
Esron Silva <esron.dtamar@gmail.com>
2022-12-04 13:12:34 -0300

    Add 2022 day 4 part 1 solution

---
+++ year_2022/day04/part1.py
@@ -0,0 +1,19 @@
+import os
+import click
+
+
+def run():
+    with open(os.getcwd() + '/year_2022/day04/input.txt') as f:
+        contains = 0
+        for line in f:
+            bounds = []
+            for pair in line.rstrip().split(','):
+                bounds.append(tuple(int(n) for n in pair.split('-')))
+            if (bounds[0][0] <= bounds[1][0] and bounds[0][1] >= bounds[1][1]) or \
+                    (bounds[0][0] >= bounds[1][0] and bounds[0][1] <= bounds[1][1]):
+                contains += 1
+        click.echo(contains)
+
+
+if __name__ == "__main__":
+    run()
