973af0d94c51f276a5a57627ad3ee40fa5188ff8
Esron Silva <esron.dtamar@gmail.com>
2022-12-04 13:12:46 -0300

    Add 2022 day 4 part 2 solution

---
+++ year_2022/day04/part2.py
@@ -0,0 +1,18 @@
+import os
+import click
+
+
+def run():
+    with open(os.getcwd() + '/year_2022/day04/input.txt') as f:
+        overlaps = 0
+        for line in f:
+            bounds = []
+            for pair in line.rstrip().split(','):
+                bounds.append(tuple(int(n) for n in pair.split('-')))
+            if not (bounds[0][1] < bounds[1][0] or bounds[1][1] < bounds[0][0]):
+                overlaps += 1
+        click.echo(overlaps)
+
+
+if __name__ == "__main__":
+    run()
