dc2f50f99c1bf59b28578339e65ed90caed61888
Esron Silva <esron.dtamar@gmail.com>
2022-12-10 16:55:57 -0300

    Add 2022 day 10 part 1 solution

---
+++ year_2022/day10/part1.py
@@ -0,0 +1,30 @@
+import os
+import click
+
+
+timers = {'addx': 2, 'noop': 1}
+
+
+def run():
+    x = 1
+    clock = 0
+    signal_strenght_sum = 0
+    with open(os.getcwd() + '/year_2022/day10/input.txt') as f:
+        instruction = f.readline().rstrip().split(' ')
+        timer = timers[instruction[0]]
+        while(clock < 220):
+            clock += 1
+            timer -= 1
+            if clock == 20 or (clock - 20) % 40 == 0:
+                signal_strenght_sum += clock * x
+            if timer == 0:
+                if instruction[0] == 'addx':
+                    x += int(instruction[1])
+                instruction = f.readline().rstrip().split(' ')
+                timer = timers[instruction[0]]
+
+    click.echo(signal_strenght_sum)
+
+
+if __name__ == "__main__":
+    run()
