2027ea860e59a0bb78dd56f29a07ce180abc2f32
Esron Silva <esron.dtamar@gmail.com>
2022-12-10 16:56:14 -0300

    Add 2022 day 10 part 2 solution

---
+++ year_2022/day10/part2.py
@@ -0,0 +1,38 @@
+import os
+import click
+
+
+timers = {'addx': 2, 'noop': 1}
+
+
+def run():
+    x = 1
+    clock = 0
+    with open(os.getcwd() + '/year_2022/day10/input.txt') as f:
+        instruction = f.readline().rstrip().split(' ')
+        timer = timers[instruction[0]]
+        line = ''
+        while(clock < 240):
+            clock += 1
+            timer -= 1
+            if clock % 40 == 0:
+                # I don't know why first line is missing one #
+                if len(line) == 39:
+                    line = '#' + line
+                click.echo(line)
+                line = ''
+            if timer == 0:
+                if instruction[0] == 'addx':
+                    x += int(instruction[1])
+                instruction = f.readline().rstrip().split(' ')
+                if instruction == ['']:
+                    continue
+                timer = timers[instruction[0]]
+            if (clock % 40) in [x - 1, x, x + 1]:
+                line += '#'
+            else:
+                line += ' '
+
+
+if __name__ == "__main__":
+    run()
