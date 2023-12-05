5f6bbc59ec6ab79bc9cd3b8fe4c07a912e20327a
Esron Silva <essilva@redhat.com>
2023-12-05 08:13:41 -0300

    Update click

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click==8.0.4',
+        'click==8.1.7',
     ],
     entry_points={
         'console_scripts': [
